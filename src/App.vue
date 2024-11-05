78e0a72fce27850ad41854167a074a4c413947fb
Vincent <S482534829@outlook.com>
2024-11-05 22:53:43 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
+import PageSizeSelector from './components/PageSizeSelector.vue'
+
+const store = useMessageStore()
+const { message } =storeToRefs(store)
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+      <div id="flashMessage" v-if="message">
+        <h4>{{ message }}</h4>
+      </div>
+      <div class="wrapper">
+        <nav>
+          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink> |
+          <RouterLink :to="{ name: 'about' }">About</RouterLink> |
+          <RouterLink :to="{ name: 'students' }">Student</RouterLink> 
+          <PageSizeSelector />
+        </nav>
+      </div>
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style>
+#layout {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+h2 {
+  font-size: 20px;
+}
+
+@keyframes yellofade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+#flashMessage {
+  animation: yellofade 3s ease-in-out;
+}
+</style>
